f70f2b77423ec198f74db824c7472138eef118de
monochromegane <dev.kuro.obi@gmail.com>
2020-03-01 21:48:18 +0900

    Add implementation of merge and drop buckets for Add method.

--- exphist.go
+++ exphist.go
@@ -4,7 +4,7 @@ import "math"
 
 type ExpHist struct {
 	windowSize uint
-	mergeSize  uint
+	mergeSize  int
 
 	time    uint
 	total   uint
@@ -15,15 +15,68 @@ type ExpHist struct {
 func New(windowSize int, epsilon float64) *ExpHist {
 	return &ExpHist{
 		windowSize: uint(windowSize),
-		mergeSize:  uint(math.Ceil(math.Ceil(1.0/epsilon)/2.0)) + 2,
+		mergeSize:  int(math.Ceil(math.Ceil(1.0/epsilon)/2.0)) + 2,
 		last:       1,
 		buckets:    [][]uint{[]uint{}},
 	}
 }
 
 func (e *ExpHist) Add(x uint) {
+	e.time += 1
+	e.drop()
+	for i := 0; i < int(x); i++ {
+		e.total += 1
+		e.buckets[0] = append(e.buckets[0], e.time)
+
+		e.merge()
+	}
 }
 
 func (e *ExpHist) Count() float64 {
 	return 0.0
+}
+
+func (e *ExpHist) drop() {
+	if !e.hasExpiredBucket() {
+		return
+	}
+	for {
+		e.tail()
+		if !e.hasExpiredBucket() {
+			break
+		}
+	}
+}
+
+func (e *ExpHist) hasExpiredBucket() bool {
+	if e.total == 0 || e.time <= e.windowSize {
+		return false
+	}
+	expiryTime := e.time - e.windowSize
+	return e.buckets[len(e.buckets)-1][0] < expiryTime
+}
+
+func (e *ExpHist) tail() {
+	size := uint(math.Pow(2.0, float64(len(e.buckets)-1)))
+	if len(e.buckets[len(e.buckets)-1]) == 1 {
+		e.buckets = e.buckets[0 : len(e.buckets)-1]
+		e.last = uint(math.Pow(2.0, float64(len(e.buckets)-1)))
+	} else {
+		e.buckets[len(e.buckets)-1] = e.buckets[len(e.buckets)-1][1:]
+	}
+	e.total -= uint(size)
+}
+
+func (e *ExpHist) merge() {
+	for i, _ := range e.buckets {
+		if len(e.buckets[i]) < e.mergeSize {
+			continue
+		}
+		if i == len(e.buckets)-1 {
+			e.buckets = append(e.buckets, []uint{})
+			e.last = uint(math.Pow(2.0, float64(len(e.buckets)-1)))
+		}
+		e.buckets[i+1] = append(e.buckets[i+1], e.buckets[i][1])
+		e.buckets[i] = e.buckets[i][2:]
+	}
 }
--- exphist_test.go
+++ exphist_test.go
@@ -1,15 +1,68 @@
 package exphist
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNew(t *testing.T) {
-	e := New(1, 0.5)
-	if e.mergeSize != 3 {
-		t.Errorf("ExpHist should have %d as mergeSize, but %d", 3, e.mergeSize)
+	hist := New(1, 0.5)
+	if hist.mergeSize != 3 {
+		t.Errorf("ExpHist should have %d as mergeSize, but %d", 3, hist.mergeSize)
 	}
 
-	e = New(1, 0.01)
-	if e.mergeSize != 52 {
-		t.Errorf("ExpHist should have %d as mergeSize, but %d", 52, e.mergeSize)
+	hist = New(1, 0.01)
+	if hist.mergeSize != 52 {
+		t.Errorf("ExpHist should have %d as mergeSize, but %d", 52, hist.mergeSize)
+	}
+}
+
+func TestAddBits(t *testing.T) {
+	hist := New(10, 0.5)
+	stream := []uint{1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0}
+	for _, x := range stream {
+		hist.Add(x)
+	}
+
+	if hist.time != uint(len(stream)) {
+		t.Errorf("Time should be %d, but %d", len(stream), hist.time)
+	}
+	if hist.total != 8 {
+		t.Errorf("Total should be %d, but %d", 8, hist.total)
+	}
+	if hist.last != 4 {
+		t.Errorf("Last should be %d, but %d", 4, hist.last)
+	}
+
+	// Expectation of the buckets
+	// Bucket:      [    ] [  ] [ ] [ ]
+	// Bucket size:    2^2  2^1 2^0 2^0
+	// TimeStamp:      ~10  ~12 ~13 ~14
+
+	bucketNum := 0
+	for i, _ := range hist.buckets {
+		bucketNum += len(hist.buckets[i])
+	}
+	if bucketNum != 4 {
+		t.Errorf("Number of buckets should be %d, but %d", 4, bucketNum)
+	}
+
+	// 0 of 2^0
+	if time := hist.buckets[0][0]; time != 13 {
+		t.Errorf("TimeStamp at 0 of 2^0 should be %d, but %d", 13, time)
+	}
+
+	// 1 of 2^0
+	if time := hist.buckets[0][1]; time != 14 {
+		t.Errorf("TimeStamp at 1 of 2^0 should be %d, but %d", 14, time)
+	}
+
+	// 0 of 2^1
+	if time := hist.buckets[1][0]; time != 12 {
+		t.Errorf("TimeStamp at 0 of 2^1 should be %d, but %d", 12, time)
+	}
+
+	// 0 of 2^2
+	if time := hist.buckets[2][0]; time != 10 {
+		t.Errorf("TimeStamp at 0 of 2^2 should be %d, but %d", 10, time)
 	}
 }
